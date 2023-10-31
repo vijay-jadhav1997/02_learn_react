6416c1c34e184dad2815db799f727838fc8535d7
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-10-31 20:41:00 +0530

    style RestaurantCard Component

--- 05_Practice_Project/src/components/RestaurantCard.js
+++ 05_Practice_Project/src/components/RestaurantCard.js
@@ -13,24 +13,25 @@ const RestaurantCard = (props) => {
     aggregatedDiscountInfoV3, 
     sla
   } = restaurantData?.info;
+
+  const cuisinesString = cuisines.join(', ');
   return (
-    <div className="restaurantCard">
-      <div className="bannerBox">
-        <img className="w-56 rounded-md" src={CDN_URL + cloudinaryImageId} />
-        <span>{
+    <div className="flex-col w-[265px] hover:bg-blue-900 border text-white p-2 rounded-xl">
+      <div className="relative">
+        <img className="w-[260px] h-[180px] rounded-lg" src={CDN_URL + cloudinaryImageId} />
+        <span className="absolute  bottom-2 left-1 bg-indigo-950 px-2">{
           (aggregatedDiscountInfoV3?.header !== undefined ? aggregatedDiscountInfoV3?.header : "")
           + " " + 
           (aggregatedDiscountInfoV3?.subHeader !== undefined ? aggregatedDiscountInfoV3?.subHeader : "")
           }</span>
       </div>
 
-      <div className="cardDetails">
-        <h3>{name}</h3>
-        <h5>{avgRating} Star</h5>
-        <h5>{cuisines.join(', ')}</h5>
-        <h5>Location: {areaName}</h5>
-        <h4>Price: {costForTwo}</h4>
-        <h4 className="font-medium">deliver in  {sla.slaString}</h4>
+      <div className="mt-3 pl-1">
+        <h3 className="text-lg">{name.length > 28 ? name.substring(0, 27)+ "..." : name}</h3>
+        <h5>{avgRating} Star . <span className="font-medium">  {sla.slaString}</span></h5>
+        <h5 className="text-sm mt-3 text-gray-400">{cuisinesString.length > 35 ? cuisinesString.substring(0, 35)+ "..." : cuisinesString}</h5>
+        <h5 className=" text-sm mt-1 text-gray-400">Location: {areaName}</h5>
+        <h4 className="text-gray-200 my-3">Price: {costForTwo}</h4>
       </div>
     </div>
 
