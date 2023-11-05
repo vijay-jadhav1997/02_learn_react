5fd5173ef685373f3f2d5e8ad913ff0528edc2aa
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-11-05 20:53:11 +0530

    Update MenuCardBox.js
    add react-icons, add veg or nonVeg icon to MenuCardBox

--- 05_Practice_Project/src/components/MenuCardBox.js
+++ 05_Practice_Project/src/components/MenuCardBox.js
@@ -1,14 +1,21 @@
 import {MENU_IMG_URL} from "../utils/constants"
+import { BiSolidStar } from "react-icons/bi";
+import { PiSquareLogoFill } from "react-icons/pi";
+import { BiCaretUpSquare } from "react-icons/bi";
 
 
 const MenuCardBox = ({menuData})=> {
-  const {name, description, price, defaultPrice, imageId} = menuData?.card?.info
-  // console.log(menuData?.card?.info);
+  const {name, description, price, defaultPrice, imageId, itemAttribute, offerTags, ribbon} = menuData?.card?.info
+  // console.log(offerTags);
   return(
     <div className="menuCardBox py-5 flex justify-between items-center">
       <div className="menuTitleBox w-3/4">
+        <h4 className="mb-1.5 flex items-center"> 
+          {itemAttribute?.vegClassifier === 'VEG' ? (<span className="text-green-500 text-xl font-medium"><PiSquareLogoFill /></span>) : (<span className="text-rose-700 text-xl"><BiCaretUpSquare /></span>)} 
+          <span className="text-orange-300 text-sm ml-2">{ribbon.hasOwnProperty('text') ? ("⭐" + ribbon?.text) : ""}</span>
+        </h4>
         <h4 className="font-medium">{name}</h4>
-        <p className="menuPrice font-light">₹{price/100 || defaultPrice/100}</p>
+        <p className="menuPrice font-light">₹{price/100 || defaultPrice/100} <span className="text-xs p-1 bg-[#FAE8E3] ml-2 border-l-2 border-l-orange-700 text-orange-500">{offerTags !== undefined ? (offerTags[0]?.title + " | " + offerTags[0]?.subTitle) : ""}</span></p>
         <p className="menuDescription my-2 text-slate-400 text-sm">{description}</p>
       </div>
       <div className="menuImgBox w-max">
