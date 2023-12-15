a20d40ec0416a520513eb444c26b292334c3adf5
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-12-15 14:00:42 +0530

    add: higher order component as WithVegLabel

--- 05_Swiggy_Clone/src/App.js
+++ 05_Swiggy_Clone/src/App.js
@@ -1,20 +1,19 @@
-// 
+//
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, Outlet, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
-import Header from "./components/Header"
+import Header from "./components/Header";
 import Body from "./components/Body";
 // import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 // import Grocery from "./components/Grocery";
-import TailwindPractice from "../src/components/TailwindPractice"
 
 const Grocery = lazy(() => import("./components/Grocery"));
 
-const AboutUs = lazy(()=> import("./components/AboutUs"));
+const AboutUs = lazy(() => import("./components/AboutUs"));
 
 const AppLayout = () => {
   return (
@@ -22,41 +21,47 @@ const AppLayout = () => {
       <Header />
       <Outlet />
     </div>
-  )
-}
-
+  );
+};
 
 const routee = createBrowserRouter([
   {
-    path:"/",
+    path: "/",
     element: <AppLayout />,
     children: [
       {
-        path:"/",
-        element: <Body/>,
+        path: "/",
+        element: <Body />,
       },
       {
-        path:"/about",
-        element: <Suspense fallback={<h1>Loading.....!!</h1>}><AboutUs/></Suspense>,
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading.....!!</h1>}>
+            <AboutUs />
+          </Suspense>
+        ),
       },
       {
-        path:"/contact",
-        element: <ContactUs/>,
+        path: "/contact",
+        element: <ContactUs />,
       },
       {
-        path:"/grocery",
-        element: <Suspense fallback={<h1>Loading.....!!</h1>}> <Grocery/></Suspense>,
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading.....!!</h1>}>
+            {" "}
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
-        path:"/restaurants/:resId",
-        element: <RestaurantMenu/>,
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
       },
     ],
-    errorElement: <Error />
-  }
-])
+    errorElement: <Error />,
+  },
+]);
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={routee} />);
--- 05_Swiggy_Clone/src/components/MenuCategory.js
+++ 05_Swiggy_Clone/src/components/MenuCategory.js
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import MenuCardBox from "./MenuCardBox"
+import MenuCardBox from "./MenuCardBox";
 import { MdKeyboardArrowDown } from "react-icons/md";
-
 
 // export const MenuCategoryVegOnly = ({data}) => {
 //   // console.log(data);
 //   const [accordion, setAccordion] = useState(false);
-  
+
 //   const vegOnlyMenuCards = data.itemCards.filter((card) => {
 //     return(card?.card?.info?.itemAttribute?.vegClassifier === 'VEG');
 //   })
@@ -35,41 +34,53 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 //   );
 // }
 
-const MenuCategory = ({isVegInfo, data, accordion, setShowAccordionFn}) => {
+const MenuCategory = ({ isVegInfo, data, setShowAccordionFn }) => {
   // console.log(data, isVegInfo, accordion);
-  // const [accordion, setAccordion] = useState(false);
+  const [accordion, setAccordion] = useState(false);
 
   const vegOnlyMenuCards = data.itemCards.filter((card) => {
-    return(card?.card?.info?.itemAttribute?.vegClassifier === 'VEG');
-  })
-  
+    return card?.card?.info?.itemAttribute?.vegClassifier === "VEG";
+  });
+
   function handleAccordion() {
-    setShowAccordionFn();
+    // setShowAccordionFn();
+    setAccordion(!accordion);
     // console.log("Now, please don't click me without providing power!!");
   }
   return (
     <div className="px-2 py-3">
-      <div className="categoryTitleBox cursor-pointer flex justify-between my-3 text-lg font-semibold"
+      <div
+        className="categoryTitleBox cursor-pointer flex justify-between my-3 text-lg font-semibold"
         onClick={handleAccordion}
       >
-        <span>{data.title} ({isVegInfo ? vegOnlyMenuCards.length : data.itemCards.length })</span>
-        {<span className={accordion ? "rotate-180" : ""}><MdKeyboardArrowDown /></span>}
+        <span>
+          {data.title} (
+          {isVegInfo ? vegOnlyMenuCards.length : data.itemCards.length})
+        </span>
+        {
+          <span className={accordion ? "rotate-180" : ""}>
+            <MdKeyboardArrowDown />
+          </span>
+        }
       </div>
 
-      {accordion && <div className="divide-y divide-slate-400">
-        {
-          isVegInfo ?
-          vegOnlyMenuCards.map((menu) => {
-            return(<MenuCardBox key={menu?.card?.info?.id} menuData={menu}/>)
-          })
-          :
-          data.itemCards.map((menu) => {
-            return(<MenuCardBox key={menu?.card?.info?.id} menuData={menu}/>)
-          })
-        }
-      </div>}
+      {accordion && (
+        <div className="divide-y divide-slate-400">
+          {isVegInfo
+            ? vegOnlyMenuCards.map((menu) => {
+                return (
+                  <MenuCardBox key={menu?.card?.info?.id} menuData={menu} />
+                );
+              })
+            : data.itemCards.map((menu) => {
+                return (
+                  <MenuCardBox key={menu?.card?.info?.id} menuData={menu} />
+                );
+              })}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default MenuCategory;
