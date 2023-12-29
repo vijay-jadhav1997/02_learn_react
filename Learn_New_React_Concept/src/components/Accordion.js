36c7c80f9d025dbf2a4bbd1582bad4c2c6e3a836
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-12-29 14:14:23 +0530

    learn accordion

--- Learn_New_React_Concept/src/components/Accordion.js
+++ Learn_New_React_Concept/src/components/Accordion.js
@@ -4,40 +4,36 @@ import { IoMdArrowDropup } from "react-icons/io";
 
 function Accordion() {
   const [showAccordion, setShowAccordion] = useState(false);
-  const [accordion, setAccordion] = useState("accordionHidden");
-
-  function showAccordionHandler() {
-    if (showAccordion) {
-      setAccordion("accordionShow");
-      setShowAccordion(!showAccordion);
-    } else {
-      setAccordion("accordionHidden");
-      setShowAccordion(!showAccordion);
-    }
-  }
 
   return (
     <div className="accordion_container">
-      <div className="topBox" onClick={showAccordionHandler}>
-        <h5>Accordion Header</h5>
+      <div
+        className="topBox"
+        onClick={() => {
+          setShowAccordion(!showAccordion);
+        }}
+      >
+        <h2>Accordion Header</h2>
         <span>
-          {showAccordion ? <IoMdArrowDropdown /> : <IoMdArrowDropup />}
+          {!showAccordion ? <IoMdArrowDropdown /> : <IoMdArrowDropup />}
         </span>
       </div>
-      <div className={`bottomBox ${accordion}`}>
-        <div className="items">Jay Shree_Ram ✨</div>
-        <div className="items">Jay Shree_Krushna ⭐</div>
-        <div className="items">Jay Shree_Hari 💥</div>
-        <div className="items">Har Har Mahadev 🎀</div>
-        <div className="items">Jay Shree_Ram ✨</div>
-        <div className="items">Jay Shree_Krushna ⭐</div>
-        <div className="items">Jay Shree_Hari 💥</div>
-        <div className="items">Har Har Mahadev 🎀</div>
-        <div className="items">Jay Shree_Ram ✨</div>
-        <div className="items">Jay Shree_Krushna ⭐</div>
-        <div className="items">Jay Shree_Hari 💥</div>
-        <div className="items">Har Har Mahadev 🎀</div>
-      </div>
+      {showAccordion && (
+        <div className="bottomBox">
+          <div className="items">Jay Shree_Ram ✨</div>
+          <div className="items">Jay Shree_Krushna ⭐</div>
+          <div className="items">Jay Shree_Hari 💥</div>
+          <div className="items">Har Har Mahadev 🎀</div>
+          <div className="items">Jay Shree_Ram ✨</div>
+          <div className="items">Jay Shree_Krushna ⭐</div>
+          <div className="items">Jay Shree_Hari 💥</div>
+          <div className="items">Har Har Mahadev 🎀</div>
+          <div className="items">Jay Shree_Ram ✨</div>
+          <div className="items">Jay Shree_Krushna ⭐</div>
+          <div className="items">Jay Shree_Hari 💥</div>
+          <div className="items">Har Har Mahadev 🎀</div>
+        </div>
+      )}
     </div>
   );
 }
