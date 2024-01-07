0ecaffbc253a16a391971c86342f9ac4a99122bc
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-07 20:30:30 +0530

    Update Cart.js

--- 05_Swiggy_Clone/src/components/Cart.js
+++ 05_Swiggy_Clone/src/components/Cart.js
@@ -1,3 +1,18 @@
+import { useSelector } from "react-redux";
+import MenuCardBox from "./MenuCardBox";
+
 export default function Cart() {
-  return <div className="cart">This is a Cart.</div>;
+  const cartItems = useSelector((store) => store.cart.items);
+
+  return (
+    <div className="w-full h-screen flex flex-col gap-y-5 justify-center items-center text-white">
+      {cartItems.map((item, index) => {
+        return (
+          <p key={index} className="text-xl font-bold">
+            {item}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
